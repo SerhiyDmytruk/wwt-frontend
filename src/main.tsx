03da729ff5109a34846629fe28645b60e140bd47
Serhii Dmytruk <dmitruksl92@gmail.com>
2026-04-24 19:53:50 +0300

    fix: removed decomposition #2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/query'
 
 import './main.css'
-import { HomePage } from './pages/home/ui/HomePage'
+import { HomePage } from './pages/Home/ui/HomePage'
 import './shared/i18n'
 
 createRoot(document.getElementById('root')!).render(
